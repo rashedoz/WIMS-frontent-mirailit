69a3daf19d2612ed23a6d7dca7619a1ef67ba04b
saiful15 <saiful.alam15@gmail.com>
2020-12-31 14:06:26 +0600

    saiful

--- src/app/all-sim-list-for-reissue/all-sim-list-for-reissue.component.ts
+++ src/app/all-sim-list-for-reissue/all-sim-list-for-reissue.component.ts
@@ -1,6 +1,7 @@
 import { Component, TemplateRef, ViewChild, ElementRef, ViewEncapsulation, OnInit } from '@angular/core';
-import { ColumnMode } from '@swimlane/ngx-datatable';
+import { ColumnMode,SelectionType,DatatableComponent } from '@swimlane/ngx-datatable';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { Router } from '@angular/router';
 import { CommonService } from '../_services/common.service';
 import { ToastrService } from 'ngx-toastr';
@@ -26,12 +27,19 @@ export class AllSIMListForReissueComponent implements OnInit {
   page = new Page();
   emptyGuid = '00000000-0000-0000-0000-000000000000';
   rows = [];
+  tempRows = [];
   loadingIndicator = false;
   ColumnMode = ColumnMode;
+  SelectionType = SelectionType;
   scrollBarHorizontal = (window.innerWidth < 1200);
-  reissueItem;
+  @ViewChild(DatatableComponent, { static: false }) table: DatatableComponent;
+  modalConfig: any = { class: 'gray modal-lg', backdrop: 'static' };
+  modalRef: BsModalRef;
+  simObj;
+  simDetails : Array<any> = [];
 
   constructor(
+    private modalService: BsModalService,
     private confirmService: ConfirmService,
     public formBuilder: FormBuilder,
     private _service: CommonService,
@@ -70,7 +78,9 @@ export class AllSIMListForReissueComponent implements OnInit {
         this.toastr.error(res.Message, 'Error!', { closeButton: true, disableTimeOut: true });
         return;
       }
+
       this.rows = res;
+      this.tempRows = res;
       // this.page.totalElements = res.Total;
       // this.page.totalPages = Math.ceil(this.page.totalElements / this.page.size);
       setTimeout(() => {
@@ -84,6 +94,10 @@ export class AllSIMListForReissueComponent implements OnInit {
     }
     );
   }
+
+
+
+
 
 
   onFormSubmit() {
@@ -139,5 +153,49 @@ export class AllSIMListForReissueComponent implements OnInit {
   }
 
 
+  updateFilter(event) {
+    const val = event.target.value.toLowerCase();
+
+    // filter our data
+    const temp = this.tempRows.filter(function (d) {
+      return d.ICCID_no.toLowerCase().indexOf(val) !== -1 ||
+             d.sim_auto_serial_no.toLowerCase().indexOf(val) !== -1 ||
+        !val;
+    });
+
+    // update the rows
+    this.rows = temp;
+    // Whenever the filter changes, always go back to the first page
+    this.table.offset = 0;
+  }
+
+  onSelect({selected}, template: TemplateRef<any>) {
+    if(selected[0]){
+      this.simObj = selected[0];
+      this.showDetails(template);
+    }
+
+    }
+
+  modalHide() {
+    this.modalRef.hide();
+    this.simObj = null;
+    this.simDetails = [];
+  }
+
+  openModal(item, template: TemplateRef<any>) {
+    this.simObj = item;
+    this.showDetails(template);
+  }
+
+  showDetails(template){
+    this._service.get('stock/get-sim-total-reissue-history?sim='+this.simObj.id).subscribe(res => {
+      if(res.length){
+        this.simDetails = res;
+        this.modalRef = this.modalService.show(template, this.modalConfig);
+      }
+    }, err => { }
+    );
+  }
 
 }
