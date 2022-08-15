2fc4988f59783533ff30d76da0013a44645a7a44
saiful15 <saiful.alam15@gmail.com>
2022-08-15 12:49:31 +0600

    saiful

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AuthenticationService } from './../_services/authentication.service';
 import { CommonService } from './../_services/common.service';
 import { ColumnMode } from '@swimlane/ngx-datatable';
-// import * as Highcharts from 'highcharts';
-// import HC_exporting from 'highcharts/modules/exporting';
+import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
+HC_exporting(Highcharts);
 import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
 import { BlockUI, NgBlockUI } from 'ng-block-ui';
 import { Page } from '../_models/page';
-// HC_exporting(Highcharts);
 import * as moment from 'moment';
 import { HttpClient } from '@angular/common/http';
 import { BsDatepickerConfig, BsDaterangepickerConfig } from "ngx-bootstrap/datepicker";
@@ -30,16 +30,27 @@ export class HomeComponent implements OnInit {
   page = new Page();
   loadingIndicator = false;
   ColumnMode = ColumnMode;
-  // highcharts = Highcharts;
+  highcharts = Highcharts;
   @BlockUI() blockUI: NgBlockUI;
   bsConfigMonth: Partial<BsDatepickerConfig>;
-  tabType = 'All Months';
+  tabType = 'Current Month';//'All Months';
   date = new Date();
   firstDay = new Date(this.date.getFullYear(), this.date.getMonth(), 1);
   lastDay = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0);
 
   bsMonthValue: Date;
   billCollectionMethodData = null;
+
+  eligibleRetailersCount = 0;
+  retailersPeripheralsCount = null;
+  retailersBillRecords = null;
+
+  eligibleWholesalersCount = 0;
+  wholesalersPeripheralsCount = null;
+  wholesalersBillRecords = null;
+
+  pieChartOptions: any;
+  pieChartArray = [];
 
   constructor(
     private authService: AuthenticationService,
@@ -51,12 +62,35 @@ export class HomeComponent implements OnInit {
     this.page.pageNumber = 0;
     this.page.size = 6;
     this.currentUser = this.authService.currentUserDetails.value;
-    // console.log(this.currentUser);
+
+    this.bsConfigMonth = Object.assign(
+      {
+        dateInputFormat: 'MMMM',
+        adaptivePosition: true,
+        minMode :'month'
+      }
+    );
+    this.bsMonthValue = this.firstDay;
   }
 
   ngOnInit() {
 
+
+
     this.getBillCollectionMethodData();
+
+    this.getBillEligibleRetailersCountData();
+    this.getRetailersPeripheralsCountData();
+    this.getRetailersBillRecordsData();
+
+    this.getBillEligibleWholesalersCountData();
+    this.getWholesalersPeripheralsCountData();
+    this.getWholesalersBillRecordsData();
+
+    setTimeout(() => {
+      this.showPicChart();
+    }, 1000);
+
 
     // this.getSIMCount();
     // this.getDeviceCount();
@@ -73,13 +107,7 @@ export class HomeComponent implements OnInit {
 
 
 
-    this.bsConfigMonth = Object.assign(
-      {
-        dateInputFormat: 'MMMM',
-        adaptivePosition: true,
-        minMode :'month'
-      }
-    );
+
 
   }
 
@@ -91,11 +119,31 @@ export class HomeComponent implements OnInit {
       case 'All Months':
         this.tabType = type;
         this.getBillCollectionMethodData();
+
+        this.getBillEligibleRetailersCountData();
+        this.getRetailersPeripheralsCountData();
+        this.getRetailersBillRecordsData();
+
+        this.getBillEligibleWholesalersCountData();
+        this.getWholesalersPeripheralsCountData();
+        this.getWholesalersBillRecordsData();
+
+        this.showPicChart();
        break;
       case 'Current Month':
         this.bsMonthValue = this.firstDay;
         this.tabType = type;
         this.getBillCollectionMethodData();
+
+        this.getBillEligibleRetailersCountData();
+        this.getRetailersPeripheralsCountData();
+        this.getRetailersBillRecordsData();
+
+        this.getBillEligibleWholesalersCountData();
+        this.getWholesalersPeripheralsCountData();
+        this.getWholesalersBillRecordsData();
+
+        this.showPicChart();
        break;
 
     default:
@@ -113,6 +161,14 @@ export class HomeComponent implements OnInit {
     this.bsMonthValue = null
   }
   this.getBillCollectionMethodData();
+
+  this.getBillEligibleRetailersCountData();
+  this.getRetailersPeripheralsCountData();
+  this.getRetailersBillRecordsData();
+
+  this.getBillEligibleWholesalersCountData();
+  this.getWholesalersPeripheralsCountData();
+  this.getWholesalersBillRecordsData();
 }
 
 
@@ -143,50 +199,273 @@ getBillCollectionMethodData() {
   );
 }
 
+getBillEligibleRetailersCountData() {
+  this.blockUI.start('Getting Data...');
+  let obj:any ={};
 
-
-
-
-
-
-
-
-
-
-  getSIMCount() {
-    this._service.get('stock/get-current-sim-stock-history').subscribe(res => {
-      this.simStock = res;
-
-    }, err => { }
-    );
+  if( this.tabType == 'Current Month'){
+      let monthLastDate = new Date(this.bsMonthValue.getFullYear(), this.bsMonthValue.getMonth() + 1, 0);
+      obj.billing_start_date = moment(this.bsMonthValue).format('YYYY-MM-DD'),
+      obj.billing_end_date = moment(monthLastDate).format('YYYY-MM-DD')
+  } else{
+    delete obj['billing_start_date'];
+    delete obj['billing_end_date'];
   }
 
-  getDeviceCount() {
-    this._service.get('stock/get-current-device-stock-history').subscribe(res => {
-      this.deviceStock = res;
-    }, err => {}
-    );
+  this._service.get('bill-dashboard/bill-eligible-retailers-count',obj).subscribe(res => {
+    this.blockUI.stop();
+    if (!res) {
+      this.toastr.error(res.Message, 'Error!', { closeButton: true, disableTimeOut: true });
+      return;
+    }
+    this.eligibleRetailersCount = res.eligible_retailer_count;
+  }, err => {
+    this.blockUI.stop();
+    this.toastr.error(err.Msg || err, 'Error!', { closeButton: true, disableTimeOut: true });
+  }
+  );
+}
+
+getRetailersPeripheralsCountData() {
+  this.blockUI.start('Getting Data...');
+  let obj:any ={};
+
+  if( this.tabType == 'Current Month'){
+      let monthLastDate = new Date(this.bsMonthValue.getFullYear(), this.bsMonthValue.getMonth() + 1, 0);
+      obj.billing_start_date = moment(this.bsMonthValue).format('YYYY-MM-DD'),
+      obj.billing_end_date = moment(monthLastDate).format('YYYY-MM-DD')
+  } else{
+    delete obj['billing_start_date'];
+    delete obj['billing_end_date'];
   }
 
-  getBillCount() {
-    this._service.get('subscription/get-bill-counts').subscribe(res => {
-      this.billCount = res;
-    }, err => {}
-    );
+  this._service.get('bill-dashboard/retailers-peripherals-count',obj).subscribe(res => {
+    this.blockUI.stop();
+    if (!res) {
+      this.toastr.error(res.Message, 'Error!', { closeButton: true, disableTimeOut: true });
+      return;
+    }
+    this.retailersPeripheralsCount = res;
+  }, err => {
+    this.blockUI.stop();
+    this.toastr.error(err.Msg || err, 'Error!', { closeButton: true, disableTimeOut: true });
   }
-  getCustomerDueList() {
-    const obj = {
-      limit: this.page.size,
-      page: this.page.pageNumber + 1,
-      search_param:''
+  );
+}
+
+getRetailersBillRecordsData() {
+  this.blockUI.start('Getting Data...');
+  let obj:any ={};
+
+  if( this.tabType == 'Current Month'){
+      let monthLastDate = new Date(this.bsMonthValue.getFullYear(), this.bsMonthValue.getMonth() + 1, 0);
+      obj.billing_start_date = moment(this.bsMonthValue).format('YYYY-MM-DD'),
+      obj.billing_end_date = moment(monthLastDate).format('YYYY-MM-DD')
+  } else{
+    delete obj['billing_start_date'];
+    delete obj['billing_end_date'];
+  }
+
+  this._service.get('bill-dashboard/retailers-bill-records',obj).subscribe(res => {
+    this.blockUI.stop();
+    if (!res) {
+      this.toastr.error(res.Message, 'Error!', { closeButton: true, disableTimeOut: true });
+      return;
+    }
+    this.retailersBillRecords = res;
+  }, err => {
+    this.blockUI.stop();
+    this.toastr.error(err.Msg || err, 'Error!', { closeButton: true, disableTimeOut: true });
+  }
+  );
+}
+
+
+getBillEligibleWholesalersCountData() {
+  this.blockUI.start('Getting Data...');
+  let obj:any = {};
+
+  if( this.tabType == 'Current Month'){
+      let monthLastDate = new Date(this.bsMonthValue.getFullYear(), this.bsMonthValue.getMonth() + 1, 0);
+      obj.billing_start_date = moment(this.bsMonthValue).format('YYYY-MM-DD'),
+      obj.billing_end_date = moment(monthLastDate).format('YYYY-MM-DD')
+  } else{
+    delete obj['billing_start_date'];
+    delete obj['billing_end_date'];
+  }
+
+  this._service.get('bill-dashboard/bill-eligible-wholesalers-count',obj).subscribe(res => {
+    this.blockUI.stop();
+    if (!res) {
+      this.toastr.error(res.Message, 'Error!', { closeButton: true, disableTimeOut: true });
+      return;
+    }
+    this.eligibleWholesalersCount = res.eligible_wholesaler_count;
+  }, err => {
+    this.blockUI.stop();
+    this.toastr.error(err.Msg || err, 'Error!', { closeButton: true, disableTimeOut: true });
+  }
+  );
+
+}
+
+getWholesalersPeripheralsCountData() {
+  this.blockUI.start('Getting Data...');
+  let obj:any ={};
+
+  if( this.tabType == 'Current Month'){
+      let monthLastDate = new Date(this.bsMonthValue.getFullYear(), this.bsMonthValue.getMonth() + 1, 0);
+      obj.billing_start_date = moment(this.bsMonthValue).format('YYYY-MM-DD'),
+      obj.billing_end_date = moment(monthLastDate).format('YYYY-MM-DD')
+  } else{
+    delete obj['billing_start_date'];
+    delete obj['billing_end_date'];
+  }
+
+  this._service.get('bill-dashboard/wholesalers-peripherals-count',obj).subscribe(res => {
+    this.blockUI.stop();
+    if (!res) {
+      this.toastr.error(res.Message, 'Error!', { closeButton: true, disableTimeOut: true });
+      return;
+    }
+    this.wholesalersPeripheralsCount = res;
+  }, err => {
+    this.blockUI.stop();
+    this.toastr.error(err.Msg || err, 'Error!', { closeButton: true, disableTimeOut: true });
+  }
+  );
+
+}
+
+getWholesalersBillRecordsData() {
+  this.blockUI.start('Getting Data...');
+  let obj:any ={};
+
+  if( this.tabType == 'Current Month'){
+      let monthLastDate = new Date(this.bsMonthValue.getFullYear(), this.bsMonthValue.getMonth() + 1, 0);
+      obj.billing_start_date = moment(this.bsMonthValue).format('YYYY-MM-DD'),
+      obj.billing_end_date = moment(monthLastDate).format('YYYY-MM-DD')
+  } else{
+    delete obj['billing_start_date'];
+    delete obj['billing_end_date'];
+  }
+
+  this._service.get('bill-dashboard/wholesalers-bill-records',obj).subscribe(res => {
+    this.blockUI.stop();
+    if (!res) {
+      this.toastr.error(res.Message, 'Error!', { closeButton: true, disableTimeOut: true });
+      return;
+    }
+    this.wholesalersBillRecords = res;
+  }, err => {
+    this.blockUI.stop();
+    this.toastr.error(err.Msg || err, 'Error!', { closeButton: true, disableTimeOut: true });
+  }
+  );
+
+}
+
+
+showPicChart() {
+  let arr = [];
+  // data.forEach((element) => {
+  //   arr.push({
+  //     'name': element.AgeRange + '(' + element.Count + ')',
+  //     'y': element.Count
+  //   });
+  // });
+
+  if(this.retailersBillRecords && this.wholesalersBillRecords){
+
+    let collectableAmount = this.retailersBillRecords.retailer_collectable_bill_amount + this.wholesalersBillRecords.wholesaler_collectable_bill_amount;
+    let collectedAmount = this.retailersBillRecords.retailer_collected_bill_amount + this.wholesalersBillRecords.wholesaler_collected_bill_amount;
+    let due = collectableAmount - collectedAmount;
+
+    arr.push({
+      'name': 'Collectable Amount',
+      'y': collectableAmount
+    });
+
+    arr.push({
+      'name': 'Collected Amount',
+      'y': collectedAmount
+    });
+
+    arr.push({
+      'name': 'Due',
+      'y': due
+    });
+
+
+
+    this.pieChartOptions = {
+      chart: {
+        plotBorderWidth: null,
+        margin: [0, 0, 50, 0],
+        spacingTop: 0,
+        spacingBottom: 0,
+        spacingLeft: 0,
+        spacingRight: 0,
+        height: 240,
+        plotShadow: false
+      },
+      exporting: {
+        fallbackToExportServer: false
+      },
+      title: {
+        text: ''
+      },
+      tooltip: {
+        pointFormat: '<b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          size: '190',
+          dataLabels: {
+            enabled: false,
+            formatter: function () {
+              return Math.round(this.percentage * 100) / 100 + ' %';
+            },
+            distance: -20,
+            color: 'white'
+          },
+          showInLegend: true,
+
+        }
+      },
+      credits: {
+        enabled: false
+      },
+      legend: {
+        symbolRadius: 0,
+        align: 'left',
+        x: 10,
+      },
+      series: [{
+        type: 'pie',
+       //name: 'Amount Collection Pie Chart',
+        data: arr,
+        colors: ['#3498db', '#2ecc71', '#c0392b'],
+        dataLabels: {
+          enabled: true,
+          formatter: function () {
+            return Math.round(Math.round(this.percentage * 100) / 100) + ' %';
+          },
+          distance: -20,
+          color: 'black',
+          style: {
+            textOutline: false
+          }
+        }
+      }
+      ]
     };
-    this._service.get('get-customer-list-with-due',obj).subscribe(res => {
-      this.customerDueList = res.results;
-      this.page.totalElements = res.count;
-      this.page.totalPages = Math.ceil(this.page.totalElements / this.page.size);
-     // console.log(this.customerDueList);
-    }, err => {}
-    );
   }
+}
+
+
+
 
 }
